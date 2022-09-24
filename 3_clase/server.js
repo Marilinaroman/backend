88b0840d3bdb6393be2e41a2ca9ef6dff81f08dc
Marilina Roman <roman.marilinaf@gmail.com>
2022-09-24 16:30:26 -0300

    desafio servidor con express

---
+++ 3_clase/server.js
@@ -0,0 +1,29 @@
+const Contenedor = require('../2_clase/archivos-sync');
+const express = require('express')
+
+//Crea servidor
+const app = express()
+
+//Configura el servidor
+
+const productos = new Contenedor('../2_clase/archivo.txt');
+
+app.get('/', (request,response)=>{
+    response.send('Hola!')
+})
+app.get('/productos', async (request,response)=>{
+    const data = await productos.getAll()
+    response.send(data)
+})
+app.get('/productosRandom', async (request,response)=>{
+    const data = productos.getAll()
+    let numero = Math.round(Math.random(1,data.length)*10)
+    const idRandom = await productos.getById(numero)
+    response.send(idRandom)
+})
+
+// Levanta el servidor
+const PORT = 8080
+const server = app.listen(PORT, () =>{
+    console.log('algo')
+})
