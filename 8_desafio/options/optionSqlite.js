cb302181969aa2d8a88f82f020f7997d03d38621
Marilina Roman <roman.marilinaf@gmail.com>
2022-11-05 17:29:11 -0300

    desafio con sqlite y mariaDb

--- 8_desafio/options/optionSqlite.js
+++
@@ -1,8 +0,0 @@
-export const options ={
-    client:"sqlite",
-    connection:{
-        filename:"./db/mensajes.sqlite"
-        
-    },
-    useNullAsDefault: true
-}
---
+++ 8_desafio/src/router/ruta.js
@@ -0,0 +1,88 @@
+import express from 'express'
+import ContendorMariaDb from '../clases/mariaDb.model.js'
+import ContenedorSqlite from '../clases/sqlite.model.js'
+import { options } from '../config/configSql.js'
+
+
+const router = express.Router()
+const productos = new ContendorMariaDb(options.mariaDb,'productos')
+const mensajes = new ContenedorSqlite(options.sqlite,'mensajes')
+
+router.get('/', async(req,res)=>{
+    res.render('form')
+})
+
+router.get('/productos', async (req,res)=>{
+    const data = await productos.getAll()
+    console.log(data);
+    res.send(data)
+})
+
+router.get('/productos/:id', async (req,res)=>{
+    const {id} = req.params
+    const producto = await productos.getById(id)
+
+    if(producto){
+        res.send(producto)
+    }else{
+        return res.json({
+            message:"el producto no existe"
+        })
+    }
+})
+
+router.post('/productos', async (req,res)=>{
+    
+    const newProd = (req.body)
+    await productos.save(newProd)
+    const data = await productos.getAll()
+    
+    res.send(data)
+
+})
+
+router.put('/productos/:id', async(req,res) =>{
+    const {id} = req.params
+    const modificacion = req.body
+    
+    const existe = await productos.getById(id)
+        
+        if (!existe){
+            return res.status(404).send({ message: 'Error el producto no existe' })
+        } else{
+            const prod = await productos.putById(id,modificacion)
+            return res.send(prod)
+        }
+    
+})
+
+router.delete('/productos/:id', async(req,res)=>{
+    const {id} = req.params
+
+    
+        const existe = await productos.getById(id)
+        
+        if (!existe){
+            return res.status(404).send({ message: 'Error el producto no existe' })
+        } else{
+            const prod = productos.deleteById(id)
+        res.send(prod)
+        }
+    
+    
+})
+
+// rutas del chat
+router.get('/chat', async(req,res)=>{
+    const data = await mensajes.getAll()
+    res.send(data)
+})
+
+router.post('/chat', async (req,res)=>{
+    const newMsj = (req.body)
+    const msj = await mensajes.save(newMsj)
+    res.send(msj)
+
+})
+
+export default router
