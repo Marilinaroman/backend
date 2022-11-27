fe6c90d3ad031194e7ab53cc65ff7adc976ac6ea
Marilina Roman <roman.marilinaf@gmail.com>
2022-11-27 10:41:18 -0300

    desafio cookies

---
+++ 12_desafio/src/server.js
@@ -0,0 +1,91 @@
+import express from 'express';
+import router from './router/ruta.js';
+import handlebars from 'express-handlebars'
+import {Server} from 'socket.io';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { contenedorMsj } from './clases/contenedorMsj.js';
+import { options } from './config/configSql.js';
+import { normalize, schema, denormalize } from "normalizr";
+import fs from "fs"
+import { readFile } from 'fs/promises'
+
+const mensajes = new contenedorMsj(options.fileSystem.pathMensajes)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// configuro archivos json
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'/views')))
+
+
+//motor plantilla
+app.engine('handlebars', handlebars.engine())
+
+// defino directorio
+app.set('views',path.join(__dirname,'/views'))
+
+//defino motor express
+app.set('view engine', 'handlebars')
+
+//normalizacion de datos
+
+//autor
+const autorSchema = new schema.Entity('autor')
+//msj
+const msjSchema = new schema.Entity('mensajes',{autor:autorSchema})
+//esquema global
+const chatSchema = new schema.Entity('chat',{
+    mensajes:[msjSchema]
+},{idAttribute:'id'})
+
+//aplico normalizacion
+const normalizarData = (data)=>{
+    const normalizacion = normalize({id:'chatHistorico', mensajes:data},chatSchema)
+    return normalizacion
+}
+
+const normalizarMsj = async()=>{
+    const resultado = await mensajes.getAll()
+    const mensajesNormalizados = normalizarData(resultado)
+    console.log(JSON.stringify(mensajesNormalizados, null,"\t"))
+    return mensajesNormalizados
+}
+normalizarMsj()
+
+
+// defino rutas
+
+app.use('/api', router);
+
+//configuro el puerto
+const PORT = process.env.PORT|| 8080
+
+const server = app.listen(PORT,()=>console.log(`server ${PORT}`))
+
+const io = new Server(server);
+
+io.on('connection', async(socket)=>{
+    console.log('nuevo usuario', socket.id)
+
+    //io.sockets.emit('productos', productos);
+	io.sockets.emit('chat', await normalizarMsj());
+
+    socket.broadcast.emit('nuevoUsuario')
+
+    /*socket.on('nuevoProducto', nuevoProducto=>{
+        productos.push(nuevoProducto)
+        fs.writeFileSync('./archivo.txt', JSON.stringify(productos))
+        io.sockets.emit('lista', productos)
+    })*/
+
+    socket.on('nuevoMsj', async (nuevoMsj) =>{
+        await mensajes.save(nuevoMsj)
+        io.sockets.emit('chat', await normalizarMsj())
+    })
+})
+
